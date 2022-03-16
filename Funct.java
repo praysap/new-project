a8a6b7db9362272790cbe79c9526f08c9e01acdd
praysap <101565426+praysap@users.noreply.github.com>
2022-03-16 14:57:12 +0530

    Add files via upload

---
+++ Funct.java
@@ -0,0 +1,22 @@
+class Funct{
+
+
+
+public static void main(String ar[])
+{ add(2,8);
+}
+
+
+
+static void add(int a,int b)
+{
+System.out.println("hello add function");
+int sum=a+b;
+System.out.println(sum);
+//return sum;
+}
+
+
+
+
+}
